fdecdee6dd66c2fb0b73731843c2ba1c11ce6788
rob121 <7796767+rob121@users.noreply.github.com>
2022-04-15 15:39:08 -0400

    Update cmp-requirements.sh

--- cmp-requirements.sh
+++ cmp-requirements.sh
@@ -1,3 +1,4 @@
 ansible-galaxy install geerlingguy.php
+ansible-galaxy install geerlingguy.php-versions
 ansible-galaxy install maxhoesel.caddy.caddy_server
 ansible-galaxy install geerlingguy.mysql
